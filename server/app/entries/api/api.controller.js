1c692a18569a54d326a31cf2a7b7c1a96ef2907f
larichev_ev <larichev_ev@ascon.net>
2023-07-30 17:26:58 +0300

    change messages

--- server/app/entries/api/api.controller.js
+++ server/app/entries/api/api.controller.js
@@ -2,12 +2,19 @@
 const { signal } = require('../../socket');
 const replay = require('../replayHelpers');
 
+const TABLE_LOOKUP = {
+  "1": "player 1",
+  "2": "player 2",
+  "3":"second service",
+  "4": "smash",
+  "5": "challenge"
+}
 async function mirrorButtonClick(req, res) {
     try {
     const { buttonId } = req.params;
 
       replay.successWithData(res, 'Button', buttonId);
-      signal('Button'+ buttonId)
+      signal('Button'+ (TABLE_LOOKUP[buttonId] ? TABLE_LOOKUP[buttonId] :  buttonId))
       console.log('Была нажата кнопка ', buttonId)
     } catch (err) {
       // eslint-disable-next-line
